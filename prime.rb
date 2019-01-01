d4b33c564b123cdb190a91c1f12de033660cdf02
Zev Spilman <zevspilman@yahoo.com>
2019-01-01 18:44:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,2 @@
-# Add  code here!
-
-  
-  def prime?(int)
-    
-   if  my_number * my_number == int
-    return false
-  else my_number = rand(1..100)
-  end
-  return true
-end
-    
+def prime?(int)
+  if 
